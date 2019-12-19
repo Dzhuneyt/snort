b8f37ef39746ca2f5cb1b7876507cc49d8e3cee2
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-12-19 23:31:39 +0200

    feat(infrastructure): Connect the app's VPC to the RDS VPC

--- terraform/service/rds.tf
+++ terraform/service/rds.tf
@@ -1,6 +1,9 @@
+data aws_vpc rds {
+  id = "vpc-24d08740"
+}
 resource "aws_vpc_peering_connection" "foo" {
   # RDS VPC
-  peer_vpc_id = "vpc-24d08740"
+  peer_vpc_id = data.aws_vpc.rds.id
   vpc_id = data.aws_vpc.selected.id
   auto_accept = true
 
@@ -23,20 +26,6 @@ data aws_route_tables snort {
 resource "aws_route" "this" {
   count = length(data.aws_route_tables.snort.ids)
   route_table_id = sort(data.aws_route_tables.snort.ids)[count.index]
-  destination_cidr_block = "172.31.0.0/16"
+  destination_cidr_block = data.aws_vpc.rds.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
 }
-//
-//resource "aws_route_table" "priv1" {
-//  vpc_id = data.aws_vpc.selected.id
-//
-//  route {
-//    # RDS CIRD block
-//    cidr_block = "172.31.0.0/16"
-//    vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
-//  }
-//
-//  tags = {
-//    Name = "aws_route_table"
-//  }
-//}
