b0de07d56b4f5c17b16f5bf3ca5595b0bf302996
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2020-01-22 17:49:52 +0200

    chore: Testing with GitHub actions

--- terraform/01_base/iam.tf
+++ terraform/01_base/iam.tf
@@ -6,7 +6,7 @@ resource "aws_iam_user" "github" {
     tag-key = "tag-value"
   }
 }
-data "aws_iam_policy_document" "github" {
+data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = [
       "sts:AssumeRole"
@@ -20,9 +20,23 @@ data "aws_iam_policy_document" "github" {
     }
   }
 }
+data "aws_iam_policy_document" "inline_policy" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+resource "aws_iam_role_policy" "this" {
+  policy = data.aws_iam_policy_document.inline_policy.json
+  role = aws_iam_role.github.id
+}
 resource "aws_iam_role" "github" {
   name = "github"
-  assume_role_policy = data.aws_iam_policy_document.github.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 resource "aws_iam_access_key" "this" {
