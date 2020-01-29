abc60c60b4d21288ba5916318aee986257569319
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2020-01-29 11:25:38 +0200

    feat: GitHub actions

--- .github/utils/terragrunt.sh
+++ .github/utils/terragrunt.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# Install Homebrew
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+set -e
 
-# Install Terragrunt & Terraform Version Manager
-brew install --force terragrunt tfenv
-brew link --overwrite terraform
+TERRAGRUNT_URL="https://github.com/gruntwork-io/terragrunt/releases/download/v0.21.11/terragrunt_linux_amd64"
+curl -O terragrunt ${TERRAGRUNT_URL}
+chmod +x terragrunt
 
 terragrunt -v
-terraform -v
