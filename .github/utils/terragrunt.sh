4acd5ee10af83598d13f430bc29c3fb3e669640d
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2020-01-29 11:52:47 +0200

    feat: GitHub actions

--- .github/utils/terragrunt.sh
+++ .github/utils/terragrunt.sh
@@ -5,6 +5,6 @@ set -e
 TERRAGRUNT_URL="https://github.com/gruntwork-io/terragrunt/releases/download/v0.21.11/terragrunt_linux_amd64"
 wget --timestamping -O terragrunt ${TERRAGRUNT_URL}
 chmod +x terragrunt
-terragrunt -v
 
 mv terragrunt /usr/local/bin
+terragrunt -v
