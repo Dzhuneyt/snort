3cf044d2b199279c06a6f3feece96951ca47f47c
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2020-01-29 11:05:55 +0200

    feat: GitHub actions

--- .github/utils/terragrunt.sh
+++ .github/utils/terragrunt.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+
+# Install Homebrew
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+# Install Terraform Version Manager
+brew install tfenv
+
+# Install latest Terraform
+tfenv install latest
+
+# Check Terraform version
+terraform -v
