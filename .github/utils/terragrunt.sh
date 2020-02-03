a5ed4398fa7309897564af63dd215b25510013c3
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2020-02-03 17:43:13 +0200

    refactor: Revert from Terragrunt path. Unnecessary complexity. Now it's back to pure Terraform again

--- .github/utils/terragrunt.sh
+++ .github/utils/terragrunt.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-TERRAGRUNT_URL="https://github.com/gruntwork-io/terragrunt/releases/download/v0.21.11/terragrunt_linux_amd64"
-wget --timestamping -O terragrunt ${TERRAGRUNT_URL}
-chmod +x terragrunt
-
-sudo mv terragrunt /usr/local/bin
-terragrunt -v
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+~/.tfenv/tfenv --version
