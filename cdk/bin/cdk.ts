f0a3a9ebdb07c4eac86123db72209bb871380b01
dzhuneyt <dzhuneyt@dzhuneyt.com>
2020-03-14 12:11:46 +0200

    ci: CodePipeline to automatically deploy the app

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,11 +2,24 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import {Snort} from "../lib/Snort";
+import {Ci} from "../lib/Ci";
 
 const app = new cdk.App();
+const environmentName = process.env.STAGE;
 
-new Snort(app, 'snort-app', {
-    description: 'Snort app',
+if (!environmentName) {
+    throw new Error('STAGE environment variable parameter not present. Can not deploy');
+}
+
+new Ci(app, `snort-ci-${environmentName}`, {
+    description: 'Snort - Continuous Integration pipelines',
+    env: {
+        region: 'us-east-1',
+    }
+});
+
+new Snort(app, `snort-app-${environmentName}`, {
+    description: 'Snort - the app itself',
     env: {
         region: 'us-east-1',
     }
---
+++ cdk/lib/Ci.ts
@@ -0,0 +1,125 @@
+import {Construct, SecretValue, Stack, StackProps} from "@aws-cdk/core";
+import {GitHubTrigger} from "@aws-cdk/aws-codepipeline-actions";
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
+import {BuildSpec, LinuxBuildImage, PipelineProject} from "@aws-cdk/aws-codebuild";
+
+export class Ci extends Stack {
+
+    private codeBuildActions: {
+        cdkBuild: PipelineProject,
+    };
+
+    private artifacts: {
+        sourceOutput: codepipeline.Artifact,
+        cdkDeployOutput: codepipeline.Artifact,
+    };
+
+    constructor(scope: Construct, id: string, props: StackProps) {
+        super(scope, id, props);
+        this.createArtifacts();
+        this.createCodeBuildActions();
+        this.createPipelines();
+    }
+
+    private createCodeBuildActions() {
+        const cdkBuild = new PipelineProject(this, 'CdkBuild', {
+            buildSpec: BuildSpec.fromObject({
+                version: '0.2',
+                phases: {
+                    install: {
+                        commands: 'cd cdk && npm i --no-audit',
+                    },
+                    build: {
+                        commands: [
+                            'ls'
+                        ],
+                    },
+                },
+            }),
+            environment: {
+                buildImage: LinuxBuildImage.STANDARD_2_0,
+            },
+        });
+
+        this.codeBuildActions = {
+            cdkBuild,
+        }
+    }
+
+    private createArtifacts() {
+        const sourceOutput = new codepipeline.Artifact();
+        const cdkDeployOutput = new codepipeline.Artifact('cdk_deploy');
+        this.artifacts = {
+            sourceOutput,
+            cdkDeployOutput
+        };
+    }
+
+    private createPipelines() {
+        const staging = new codepipeline.Pipeline(this, 'staging', {
+            stages: [
+                {
+                    stageName: 'Source',
+                    actions: [
+                        new codepipeline_actions.GitHubSourceAction({
+                            branch: 'develop',
+                            actionName: "Source",
+                            oauthToken: SecretValue.secretsManager('GITHUB_TOKEN'),
+                            owner: 'Dzhuneyt',
+                            repo: 'snort',
+                            trigger: GitHubTrigger.NONE,
+                            output: this.artifacts.sourceOutput,
+                        }),
+                    ],
+                },
+                {
+                    stageName: 'Build',
+                    actions: [
+                        new codepipeline_actions.CodeBuildAction({
+                            actionName: 'CDK_Deploy',
+                            project: this.codeBuildActions.cdkBuild,
+                            input: this.artifacts.sourceOutput,
+                            outputs: [this.artifacts.cdkDeployOutput],
+                        }),
+                    ],
+                },
+            ],
+        });
+
+        const production = new codepipeline.Pipeline(this, 'production', {
+            stages: [
+                {
+                    stageName: 'Source',
+                    actions: [
+                        new codepipeline_actions.GitHubSourceAction({
+                            branch: 'master',
+                            actionName: "Source",
+                            oauthToken: SecretValue.secretsManager('GITHUB_TOKEN'),
+                            owner: 'Dzhuneyt',
+                            repo: 'snort',
+                            trigger: GitHubTrigger.NONE,
+                            output: this.artifacts.sourceOutput,
+                        }),
+                    ],
+                },
+                {
+                    stageName: 'Build',
+                    actions: [
+                        new codepipeline_actions.CodeBuildAction({
+                            actionName: 'CDK_Deploy',
+                            project: this.codeBuildActions.cdkBuild,
+                            input: this.artifacts.sourceOutput,
+                            outputs: [this.artifacts.cdkDeployOutput],
+                        }),
+                    ],
+                },
+            ],
+        });
+
+        return {
+            staging,
+            production,
+        }
+    }
+}
