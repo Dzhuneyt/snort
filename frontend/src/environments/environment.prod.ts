dd7acdcd825e7e26645994dc1a1a350efa8e2b12
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2020-03-15 15:54:08 +0200

    ci: Build frontend and pass backend URL to it

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
-if (!process.env.BACKEND_URL) {
-  throw new Error('Can not build frontend without BACKEND_URL environment variable');
-}
 export const environment = {
   production: true,
   backend: '${BACKEND_URL}',
